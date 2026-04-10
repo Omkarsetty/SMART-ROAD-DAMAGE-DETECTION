5e5a3594f602bf6d946df4c60d59595e7b5b73a3
omkar shetty <155879769+Omkarsetty@users.noreply.github.com>
2026-04-10 22:41:51 +0530

    Update copy_assets - Copy.bat

--- copy_assets - Copy.bat
+++ copy_assets - Copy.bat
@@ -1,12 +1,1 @@
-@echo off
-set SRC=C:\Users\medis\.gemini\antigravity\brain\bacf6f03-8dcb-4633-8b70-43d7a4c76d2f
-set DEST=c:\Users\medis\Downloads\h\public\dataset
 
-if not exist "%DEST%" mkdir "%DEST%"
-
-copy /y "%SRC%\pothole_1_1775807607492.png" "%DEST%\pothole_1.png"
-copy /y "%SRC%\crack_1_1775807629775.png" "%DEST%\crack_1.png"
-copy /y "%SRC%\road_view_1_1775807651473.png" "%DEST%\road_view_1.png"
-
-echo Done.
-dir "%DEST%"
